db1d956a17d54189fd2e85b4254806c6a940a82b
Ryan Reeves <reeves0ryan@gmail.com>
2022-09-20 23:14:59 -0700

    gh-pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -74,9 +74,6 @@ export default function App() {
     handleCaseChange();
   };
   
-  // const renderKeyboard = () => {
-  //   return lowercaseKeyboard;
-  // }
   useEffect(() => {
     let renderedRows = document.querySelectorAll(".row");
     let backspaceKey = document.querySelector(".backspace-key");
