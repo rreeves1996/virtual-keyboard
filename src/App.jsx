467100056bb2b7897ae3ef39888ffdc20d9ad919
Ryan Reeves <reeves0ryan@gmail.com>
2022-09-20 21:01:29 -0700

    Capslock and shift work!

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import { render } from '@testing-library/react';
 
 export default function App() {
   const [text, addText] = useState('');
-  const characters = `1234567890-= qwertyuiop[] asdfghjkl;' zxcvbnm,./`;
+  const [uppercase, toggleCaps] = useState(false);
+  const [capslock, toggleCapslock] = useState(false);
 
+  const characters = `1234567890-= qwertyuiop[] asdfghjkl;' zxcvbnm,./`;
+  const characterGroup = characters.split(' ');
   let rows = [, , ,];
-  const backspaceKeyElement = 
+  const backspaceElement = 
   <>
     <button className='key backspace-key' onClick={() => addText(text.slice(0, -1))}><i className="fa-solid fa-arrow-left-long"></i></button>
   </>;
@@ -16,22 +19,74 @@ export default function App() {
   <>
     <button className='key spacebar-key' onClick={() => handleAddText(' ')}><div className="spacebar-icon">]</div></button>
   </>;
-  const characterGroup = characters.split(' ');
-  
+  const capslockElement = 
+  <>
+    <button className='key capslock-key' onClick={() => handleCapsLock()}>caps</button>
+  </>;
+  const shiftElement = 
+  <>
+    <button className='key shift-key' onClick={() => handleCaseChange()}>shift</button>
+  </>;
+
   characterGroup.forEach((row, index) => {
     rows[index] = row.split('');
   })
 
   const handleAddText = (event) => {
-    addText(text + event);
-    console.log(event);
+    if(uppercase){
+      let uppercaseText = event.toUpperCase();
+      console.log(uppercaseText);
+      addText(text + event.toUpperCase());
+      if(!capslock) {
+        handleCaseChange();
+      }
+    } else {
+      addText(text + event);
+    }
   }
 
+  const handleCaseChange = () => {
+    if(!uppercase) {
+      let lowercaseKey = document.querySelectorAll('.lowercase-key');
+
+      lowercaseKey.forEach(key => {
+        key.innerHTML = key.innerHTML.toUpperCase();
+
+        key.removeAttribute('lowercase-key');
+        key.setAttribute('class', 'key uppercase-key');
+      })
+      toggleCaps(!uppercase);
+    } else {
+      let uppercaseKey = document.querySelectorAll('.uppercase-key');
+
+      uppercaseKey.forEach(key => {
+        key.innerHTML = key.innerHTML.toLowerCase();
+
+        key.removeAttribute('uppercase-key');
+        key.setAttribute('class', 'key lowercase-key');
+      })
+      toggleCaps(!uppercase);
+    }
+  }
+
+  const handleCapsLock = () => {
+    toggleCapslock(!capslock);
+    handleCaseChange();
+  };
+  
+  // const renderKeyboard = () => {
+  //   return lowercaseKeyboard;
+  // }
   useEffect(() => {
     let renderedRows = document.querySelectorAll(".row");
-    let backspaceKey = document.querySelector(".backspace-key")
+    let backspaceKey = document.querySelector(".backspace-key");
+    let capslockKey = document.querySelector(".capslock-key");
+    let shiftKey = document.querySelector(".shift-key");
+
     renderedRows[0].append(backspaceKey);
-  })
+    renderedRows[2].prepend(capslockKey);
+    renderedRows[3].prepend(shiftKey);
+  });
 
   return (
     <>
@@ -43,18 +98,20 @@ export default function App() {
         </header>
         <textarea className="typed-text" value={text} onChange={handleAddText}></textarea>
         <div className="keyboard">
-          {backspaceKeyElement}
+          {backspaceElement}
           {
           rows.map(row =>(
             <div className="row">
             {row.map((key) => (
-              <button className='key' onClick={() => handleAddText(key)}>{key}</button>
+              <button className='key lowercase-key' onClick={() => handleAddText(key)}>{key}</button>
             ))}
             </div>
           ))}
+          {capslockElement}
+          {shiftElement}
           {spacebarElement}
         </div>
       </main>
     </>
-  );
+  )
 }
