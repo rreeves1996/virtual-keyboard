0ca1ceaf65a7da0679eff8dfd0a9842a4cc51287
Ryan Reeves <102436216+rreeves1996@users.noreply.github.com>
2022-09-21 20:03:06 -0700

    Keyboard component, textarea component (#1)

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,89 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
+import TextArea from './TextArea';
+import Keyboard from './Keyboard';
 import '../assets/style/style.css';
 
 
 export default function App() {
-  const [text, addText] = useState('');
-  const [uppercase, toggleCaps] = useState(false);
-  const [capslock, toggleCapslock] = useState(false);
-
-  const characters = `1234567890-= qwertyuiop[] asdfghjkl;' zxcvbnm,./`;
-  const characterGroup = characters.split(' ');
-  let rows = [, , ,];
-  const backspaceElement = 
-  <>
-    <button className='key backspace-key' onClick={() => addText(text.slice(0, -1))}><i className="fa-solid fa-arrow-left-long"></i></button>
-  </>;
-  const spacebarElement = 
-  <>
-    <button className='key spacebar-key' onClick={() => handleAddText(' ')}><div className="spacebar-icon">]</div></button>
-  </>;
-  const capslockElement = 
-  <>
-    <button className='key capslock-key' onClick={() => handleCapsLock()}>caps</button>
-  </>;
-  const shiftElement = 
-  <>
-    <button className='key shift-key' onClick={() => handleCaseChange()}>shift</button>
-  </>;
-
-  characterGroup.forEach((row, index) => {
-    rows[index] = row.split('');
-  })
-
-  const handleAddText = (event) => {
-    if(uppercase){
-      let uppercaseText = event.toUpperCase();
-      console.log(uppercaseText);
-      addText(text + event.toUpperCase());
-      if(!capslock) {
-        handleCaseChange();
-      }
-    } else {
-      addText(text + event);
-    }
-  }
-
-  const handleCaseChange = () => {
-    if(!uppercase) {
-      let lowercaseKey = document.querySelectorAll('.lowercase-key');
-
-      lowercaseKey.forEach(key => {
-        key.innerHTML = key.innerHTML.toUpperCase();
-
-        key.removeAttribute('lowercase-key');
-        key.setAttribute('class', 'key uppercase-key');
-      })
-      toggleCaps(!uppercase);
-    } else {
-      let uppercaseKey = document.querySelectorAll('.uppercase-key');
-
-      uppercaseKey.forEach(key => {
-        key.innerHTML = key.innerHTML.toLowerCase();
-
-        key.removeAttribute('uppercase-key');
-        key.setAttribute('class', 'key lowercase-key');
-      })
-      toggleCaps(!uppercase);
-    }
-  }
-
-  const handleCapsLock = () => {
-    toggleCapslock(!capslock);
-    handleCaseChange();
-  };
   
-  useEffect(() => {
-    let renderedRows = document.querySelectorAll(".row");
-    let backspaceKey = document.querySelector(".backspace-key");
-    let capslockKey = document.querySelector(".capslock-key");
-    let shiftKey = document.querySelector(".shift-key");
-
-    renderedRows[0].append(backspaceKey);
-    renderedRows[2].prepend(capslockKey);
-    renderedRows[3].prepend(shiftKey);
-  });
 
   return (
     <>
@@ -93,21 +16,9 @@ export default function App() {
           <code>Type anything using the virtual keyboard</code>
           <code>(or your own keyboard)</code>
         </header>
-        <textarea className="typed-text" value={text} onChange={handleAddText}></textarea>
-        <div className="keyboard">
-          {backspaceElement}
-          {
-          rows.map(row =>(
-            <div className="row">
-            {row.map((key) => (
-              <button className='key lowercase-key' onClick={() => handleAddText(key)}>{key}</button>
-            ))}
-            </div>
-          ))}
-          {capslockElement}
-          {shiftElement}
-          {spacebarElement}
-        </div>
+        <TextArea>
+          <Keyboard />
+        </TextArea>
       </main>
     </>
   )
