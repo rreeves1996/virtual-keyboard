294f4f7504915d7edacd533ec184f987862d0b5c
Ryan Reeves <reeves0ryan@gmail.com>
2022-09-21 21:40:08 -0700

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Navbar() {
+  const [collapsed, toggleCollapse] = useState(true);
+
+
+
     return (
       <nav className='navbar'>
         <i className="fa-regular fa-keyboard"></i>
@@ -8,14 +12,15 @@ export default function Navbar() {
                 <code>React.js Virtual Keyboard</code>
                 <p>created by Ryan Reeves</p>
             </header>
-            <div className="navbar-toggler">
+            <div className={collapsed ? "navbar-toggler" : "navbar-toggler open"} onClick={() => toggleCollapse(!collapsed)}>
             <i className="fa-solid fa-bars"></i><i className="fa-solid fa-chevron-down navbar-hover-arrow"></i>
-            </div>
-            <div className="nav-link-container collapse">
+            <div className={!collapsed ? "nav-link-container" : "nav-link-container collapse"}>
                 <a href="#" className='nav-link' id='github'>Repo Link</a>
                 <a href="#" className='nav-link' id='about'>About</a>
                 <a href="#" className='nav-link' id='contact'>Contact</a>
             </div>
+            </div>
+
       </nav>
     );
   }
